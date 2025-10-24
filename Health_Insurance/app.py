5a186b14a69e561014475f1b93920b4966b8db54
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2025-10-24 16:27:08 +0530

    serving ML models with fast api

--- Health_Insurance/app.py
+++ Health_Insurance/app.py
@@ -42,14 +42,39 @@ class UserInput(BaseModel):
     sex: Annotated[str, Field(..., description='Gender of the user')]
     weight: Annotated[float, Field(..., gt=0, description='weight of the user')]
     bmi: Annotated[float, Field(..., gt=0, description='BMI of the user')]
-    hereditary_diseases: Annotated[bool, Field(..., gt=0, description='any disease user has?')]
+    hereditary_diseases: Annotated[Literal['NoDisease', 'Epilepsy', 'EyeDisease', 'Alzheimer', 'Arthritis',
+       'HeartDisease', 'Diabetes', 'Cancer', 'High BP', 'Obesity'], bool, Field(...,description='any disease user has?')]
     no_of_dependents: Annotated[int, Field(..., description='no. of people who depend on the user for livelihood')]
     smoker: Annotated[int, Field(..., description='user smokes or not')]
-    city: Annotated[str, Field(..., description='city of user')]
+    city: Annotated[Literal['NewYork', 'Boston', 'Phildelphia', 'Pittsburg', 'Buffalo',
+       'AtlanticCity', 'Portland', 'Cambridge', 'Hartford', 'Springfield',
+       'Syracuse', 'Baltimore', 'York', 'Trenton', 'Warwick',
+       'WashingtonDC', 'Providence', 'Harrisburg', 'Newport', 'Stamford',
+       'Worcester', 'Atlanta', 'Brimingham', 'Charleston', 'Charlotte',
+       'Louisville', 'Memphis', 'Nashville', 'NewOrleans', 'Raleigh',
+       'Houston', 'Georgia', 'Oklahoma', 'Orlando', 'Macon', 'Huntsville',
+       'Knoxville', 'Florence', 'Miami', 'Tampa', 'PanamaCity',
+       'Kingsport', 'Marshall', 'Mandan', 'Waterloo', 'IowaCity',
+       'Columbia', 'Indianapolis', 'Cincinnati', 'Bloomington', 'Salina',
+       'KanasCity', 'Brookings', 'Minot', 'Chicago', 'Lincoln',
+       'FallsCity', 'GrandForks', 'Fargo', 'Cleveland', 'Canton',
+       'Columbus', 'Rochester', 'Minneapolis', 'JeffersonCity',
+       'Escabana', 'Youngstown', 'SantaRosa', 'Eureka', 'SanFrancisco',
+       'SanJose', 'LosAngeles', 'Oxnard', 'SanDeigo', 'Oceanside',
+       'Carlsbad', 'Montrose', 'Prescott', 'Fresno', 'Reno', 'LasVegas',
+       'Tucson', 'SanLuis', 'Denver', 'Kingman', 'Bakersfield',
+       'Mexicali', 'SilverCity', 'Pheonix', 'SantaFe', 'Lovelock'],str, Field(..., description='city of user')]
     bloodpressure: Annotated[int, Field(..., gt=0, description='blood pressure of user')]
     diabetes: Annotated[int, Field(..., description='user has diabetes or not')]
     regular_ex: Annotated[int, Field(..., description='user do regular excercise or not')]
-    job_title: Annotated[str, Field(..., gt=0, description='job title of user')]
+    job_title: Annotated[Literal['Actor', 'Engineer', 'Academician', 'Chef', 'HomeMakers', 'Dancer',
+       'Singer', 'DataScientist', 'Police', 'Student', 'Doctor',
+       'Manager', 'Photographer', 'Beautician', 'CA', 'Blogger', 'CEO',
+       'Labourer', 'Accountant', 'FilmDirector', 'Technician',
+       'FashionDesigner', 'Architect', 'HouseKeeper', 'FilmMaker',
+       'Buisnessman', 'Politician', 'DefencePersonnels', 'Analyst',
+       'Clerks', 'ITProfessional', 'Farmer', 'Journalist', 'Lawyer',
+       'GovEmployee'],str, Field(...,description='job title of user')]
     
     
     @computed_field
