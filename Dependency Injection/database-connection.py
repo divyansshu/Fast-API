8d855202f6c7da3188ef14b92394c42c294b4aeb
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2026-02-01 16:45:43 +0530

    feat(DatabaseConnection): implement database connection dependency with mock database

---
+++ Dependency Injection/database-connection.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+def get_db():
+    db = {'connection': 'mock_db_connection'}
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/home')
+def home(db=Depends(get_db)):
+    return {'db_status': db['connection']}
+    
