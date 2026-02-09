b7eeea69a6b768a4b7398a101bdd37d273481a3f
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2026-02-09 12:51:24 +0530

    feat(LoanEligibility): add loan eligibility endpoint and corresponding tests

---
+++ Testing and Debugging/E2E_testing/tests/test_main.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_eligibility_pass():
+    response = client.post('/loan_eligibility', json = {
+        'income': 60000,
+        'age': 25,
+        'employment_status': 'employed' 
+    })
+    assert response.status_code == 200
+    assert response.json() == {'eligible': True}
+
+def test_eligibility_fail():
+    response = client.post('/loan_eligibility', json = {
+        'income': 30000,
+        'age': 19,
+        'employment_status': 'unemployed' 
+    })
+    assert response.status_code == 200
+    assert response.json() == {'eligible': False}
