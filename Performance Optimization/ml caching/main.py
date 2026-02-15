13aa5ab7ad334c5f54d703cd62510d922785294c
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2026-02-15 21:08:32 +0530

    feat(Caching): implement caching for prediction results using Redis

---
+++ Performance Optimization/ml caching/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import redis
+import json
+import joblib
+import hashlib
+
+app = FastAPI()
+redis_client = redis.Redis(host='localhost', port=6379,db=0)
+model = joblib.load('model.joblib')
+
+class Iris(BaseModel):
+    SepalLengthCm:float
+    SepalWidthCm: float
+    PetalLengthCm: float
+    PetalWidthCm: float
+    
+    def to_list(self):
+        return [
+            self.SepalLengthCm,
+            self.SepalWidthCm,
+            self.PetalLengthCm,
+            self.PetalWidthCm
+        ]
+    
+    def cache_key(self):
+        raw = json.dumps(self.model_dump(), sort_keys=True)
+        return f"Predict: {hashlib.sha256(raw.encode()).hexdigest()}"
+
+@app.post('/predict')
+async def predict(data: Iris):
+    key = data.cache_key()
+    
+    cached_result = redis_client.get(key)
+    if cached_result:
+        print('Serving prediction from Cache')
+        return json.loads(cached_result)
+    
+    prediction = model.predict([data.to_list()])[0]
+    result = {'prediction': int(prediction)}
+    redis_client.set(key, json.dumps(result), ex=3600)
+    return result
+    
